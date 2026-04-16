36e3dded6b196a9f91ee428e2542ce5b3577c1bc
tobyyho <81172666+tobyyho@users.noreply.github.com>
2026-04-16 01:36:32 -0400

    Refactor InventoryStore to extend ChangeNotifier

--- lib/models/inventory_store.dart
+++ lib/models/inventory_store.dart
@@ -1,22 +1,71 @@
+import 'package:flutter/material.dart';
 import 'clothing_item.dart';
 
-class InventoryStore {
-  static final List<ClothingItem> items = [
+class InventoryStore extends ChangeNotifier {
+  final List<ClothingItem> _items = [
     const ClothingItem(
-      id: '1',
+      id: 'demo_top_1',
       type: ClothingType.top,
-      name: 'White Button-Down',
-      color: 'White',
+      name: 'Cream Tee',
+      colorLabel: 'Cream',
     ),
     const ClothingItem(
-      id: '2',
+      id: 'demo_bottom_1',
       type: ClothingType.bottom,
-      name: 'Charcoal Trousers',
-      color: 'Charcoal',
+      name: 'Black Pants',
+      colorLabel: 'Black',
     ),
   ];
 
-  static void addItem(ClothingItem item) {
-    items.add(item);
+  String? _selectedTopId = 'demo_top_1';
+  String? _selectedBottomId = 'demo_bottom_1';
+
+  List<ClothingItem> get items => List.unmodifiable(_items);
+
+  List<ClothingItem> get tops =>
+      _items.where((item) => item.type == ClothingType.top).toList();
+
+  List<ClothingItem> get bottoms =>
+      _items.where((item) => item.type == ClothingType.bottom).toList();
+
+  List<ClothingItem> get accessories =>
+      _items.where((item) => item.type == ClothingType.accessory).toList();
+
+  ClothingItem? get selectedTop => _findById(_selectedTopId);
+  ClothingItem? get selectedBottom => _findById(_selectedBottomId);
+
+  void addItem(ClothingItem item) {
+    _items.add(item);
+    notifyListeners();
+  }
+
+  void updateItem(ClothingItem updated) {
+    final index = _items.indexWhere((item) => item.id == updated.id);
+    if (index == -1) return;
+    _items[index] = updated;
+    notifyListeners();
+  }
+
+  void selectItem(ClothingItem item) {
+    switch (item.type) {
+      case ClothingType.top:
+        _selectedTopId = item.id;
+        break;
+      case ClothingType.bottom:
+        _selectedBottomId = item.id;
+        break;
+      case ClothingType.accessory:
+        break;
+    }
+    notifyListeners();
+  }
+
+  ClothingItem? _findById(String? id) {
+    if (id == null) return null;
+    try {
+      return _items.firstWhere((item) => item.id == id);
+    } catch (_) {
+      return null;
+    }
   }
 }
